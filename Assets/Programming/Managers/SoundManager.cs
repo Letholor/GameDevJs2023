41aa9203b308eb3e43d7ecc5c2295f5436d4737d
Letholor <lee.reynolds2008@gmail.com>
2023-04-25 14:08:49 -0400

    Sound starter

---
+++ Assets/Programming/Managers/SoundManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
