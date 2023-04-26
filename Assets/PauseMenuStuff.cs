775dd1663734056a7dfafd37ee1b1bf97cb2d69d
darklaw53 <marceloschulze@gmail.com>
2023-04-26 00:22:36 -0300

    game done

---
+++ Assets/PauseMenuStuff.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuStuff : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        Time.timeScale = 0;
+        AudioListener.pause = true;
+    }
+
+    public void BackToMain()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void Close()
+    {
+        Time.timeScale = 1;
+        AudioListener.pause = false;
+    }
+}
